973ff0191cf9006b630c4254a50bcfc352c2d0d7
Ubuntu <ubuntu@ip-172-31-32-44.ap-southeast-1.compute.internal>
2023-09-12 08:44:59 +0000

    aaa

--- scan.sh
+++ scan.sh
@@ -1,2 +1,3 @@
 SONAR_TOKEN=5dfabcc2ca36e0698588365a0091fb48be487b24
-/opt/devops/sonar/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner.bat -Dproject.settings=./sonar-project.properties
+mvn clean package
+/opt/devops/sonar/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner -Dproject.settings=./sonar-project.properties
