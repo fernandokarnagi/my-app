f05ca4f3d1c026cf60800faf1b095101f8917cb4
fernando <fernando@fernando.co>
2023-09-12 18:06:23 +0800

    Added new class

---
+++ src/main/java/com/mycompany/app/NewClassFeature.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+public class NewClassFeature {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
