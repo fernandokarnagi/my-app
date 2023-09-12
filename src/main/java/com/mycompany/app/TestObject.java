97796ee005361da8f0d6a55de0bd3e522dd52404
fernando <fernando@fernando.co>
2023-09-12 17:01:57 +0800

    update

---
+++ src/main/java/com/mycompany/app/TestObject.java
@@ -0,0 +1,31 @@
+package com.mycompany.app;
+
+import java.util.Objects;
+
+public class TestObject {
+    private String name;
+
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestObject that = (TestObject) o;
+        return Objects.equals(name, that.name) && Objects.equals(address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address);
+    }
+}
+
