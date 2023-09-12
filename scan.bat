9b31b59f6f4b3203ba06ffa04fbef0f5f620b193
fernando <fernando@fernando.co>
2023-09-12 16:35:23 +0800

    update

---
+++ scan.bat
@@ -0,0 +1,2 @@
+set SONAR_TOKEN=5dfabcc2ca36e0698588365a0091fb48be487b24
+D:\App\sonar-scanner-4.7.0.2747-windows\bin\sonar-scanner.bat -Dproject.settings=./sonar-project.properties
---
+++ scan.sh
@@ -0,0 +1,2 @@
+SONAR_TOKEN=5dfabcc2ca36e0698588365a0091fb48be487b24
+/opt/devops/sonar/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner.bat -Dproject.settings=./sonar-project.properties
